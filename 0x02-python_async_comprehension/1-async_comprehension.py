72f7a71dad8d54c935738be4befd472e5436a6fd
Jason Flair <emelieobumse100@gmail.com>
2023-04-04 19:26:25 +0100

    all done now

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,7 @@ import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> typing.List['float']:
+async def async_comprehension() -> typing.List[float]:
     """function that performs async comprehension"""
     result_list = [i async for i in async_generator()]
     return result_list
