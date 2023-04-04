27942b6e41e8b8d60a37513218c9bda91a754551
Jason Flair <emelieobumse100@gmail.com>
2023-04-04 19:16:32 +0100

    all done now

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""an async generator"""
+'''Task 0's module.
+'''
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """async generator that yields random numbers
-     using a Generator type hint is because it returns
-     a Python generator object which is an iterable"""
-    for i in range(10):
+    '''Generates a sequence of 10 numbers.
+    '''
+    for _ in range(10):
         await asyncio.sleep(1)
-        num = random.uniform(0, 10)
-        yield num
+        yield random.random() * 10
