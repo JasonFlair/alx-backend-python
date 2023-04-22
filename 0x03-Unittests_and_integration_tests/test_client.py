760ea293b9fe7c1ebb36c2a1cbc430c01fc04ab5
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2023-04-22 14:18:31 +0000

    testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """testing client.py methods"""
 import unittest
-from unittest.mock import PropertyMock, patch
+from unittest.mock import MagicMock, PropertyMock, patch
 from client import *
 from parameterized import parameterized
 
@@ -11,7 +11,7 @@ class TestGithubOrgClient(unittest.TestCase):
   @parameterized.expand([("google",),
                          ("abc")])
   @patch('client.get_json')
-  def test_org(self, org, mock_getjson):
+  def test_org(self, org, mock_getjson: MagicMock) -> None:
     """test that the correct org is returned"""
     new_client = GithubOrgClient(org_name=org)
     self.assertEqual(new_client._org_name, org)
@@ -20,7 +20,7 @@ class TestGithubOrgClient(unittest.TestCase):
     mock_getjson.assert_called_once()
     
   @patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock)
-  def test_public_repos_url(self, mock_property):
+  def test_public_repos_url(self, mock_property: MagicMock) -> None:
     """mocking a @property"""
     mock_property.return_value = {"payload": True}
     self.assertEqual(mock_property.return_value, {"payload": True})
